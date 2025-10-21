7bf4c9c61b0ad11a1f6f0775b0bffbf71e5f2270
Tolben <vitek.pasek@gmail.com>
2025-10-21 02:36:25 +0200

    chore(schema): split from umbrella

---
+++ schema/020_indexes.sql
@@ -0,0 +1,3 @@
+-- Auto-generated from schema-map.psd1 on 2025-10-21T02:32:05
+-- table: key_events
+CREATE INDEX idx_key_events_basename ON key_events (basename);
---
+++ schema/030_foreign_keys.sql
@@ -0,0 +1,5 @@
+-- Auto-generated from schema-map.psd1 on 2025-10-21T02:32:05
+-- table: key_events
+ALTER TABLE key_events ADD CONSTRAINT fk_key_events_key FOREIGN KEY (key_id) REFERENCES crypto_keys(id) ON DELETE SET NULL;
+
+ALTER TABLE key_events ADD CONSTRAINT fk_key_events_actor FOREIGN KEY (actor_id) REFERENCES users(id) ON DELETE SET NULL;
